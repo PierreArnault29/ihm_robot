ff7c998ea4ff3f49879e70ae2cfd3de323847dfa
PierreArnault29 <arnault@lacroixrouge-brest.fr>
2018-11-05 16:05:52 +0100

    Creation IHM de base: 4boutons et 2 inputs text pour 2 moteurs

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+		<title>ROBOT</title>
+
+	</head>
+	<body>
+		<center>
+		<div id="titre1" class="titre" style="border:thin solid"><h1> Ma telecommande de ROBOT</h1></div>
+		</center>
+		<p> Ma télécommande</p><br>
+		<div>
+			<p>Moteur 1:</p>
+			<input type="button" value="+(Avancer)">
+			<input type="text" value="Valeur vitesse">
+			<input type="button" value="-(Reculer)">
+		</div><br>
+		<div>
+			<p>Moteur 2:</p>
+			<input type="button" value="+(Avancer)">
+			<input type="text" value="Valeur vitesse">
+			<input type="button" value="-(Reculer)">
+		</div><br>
+		<div>
+			<p>Options:</p>
+			<input type="button" value="Start">
+			<input type="button" value="Stop">
+		</div><br>		
+		<a href="#"> Mon premier lien</a>
+		<img src=""></img>
+	<?php
+		echo "Nous sommes aujourd'hui" . date("1").".";
+		?>
+	</body>
+</html>
